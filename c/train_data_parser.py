fdaddbc815beb6964123dadbee3bce6add3175ad
Nathan Thomas <nathanthomas707@gmail.com>
2024-03-21 11:19:41 -0700

    Improve train data parser

--- c/train_data_parser.py
+++ c/train_data_parser.py
@@ -1,16 +1,18 @@
 import re
 import sys
 
-path = sys.argv[1]
-
 
 def paragraphs(s):
     return s.split("\n\n")
 
 
+re_avg_max = re.compile(r"\d+\s+avg\s+=\s+(\d+\.?\d*)\s+max\s+=\s+(\d+)")
+re_tile_info = re.compile(r"\s+(\d+)\s+(\d+\.?\d*)%\s+\((\d+\.?\d*)\%\)\s*")
+
+
 def parse_paragraph(p):
     lines = p.split("\n")
-    m = re.match(r"\d+\s+avg\s+=\s+(\d+\.?\d*)\s+max\s+=\s+(\d+)", lines[0])
+    m = re_avg_max.match(lines[0])
     assert m is not None
     avg, max = float(m.group(1)), int(m.group(2))
     # 256     100%    (0.3%)
@@ -18,15 +20,20 @@ def parse_paragraph(p):
     for line in lines[1:]:
         if not line:
             continue
-        print(f"'{line}'")
-        m = re.match(r"\s+(\d+)\s+(\d+\.?\d*)%\s+\((\d+\.?\d*)\%\)\s*", line)
+        m = re_tile_info.match(line)
         assert m is not None
         tilen, perc, perc2 = int(m.group(1)), float(m.group(2)), float(m.group(3))
         items.append((tilen, perc, perc2))
     return avg, max, items
 
 
-with open(path) as f:
-    s = f.read()
-    info = [parse_paragraph(p) for p in paragraphs(s)]
-    print(info)
+def main(path):
+    with open(path) as f:
+        s = f.read()
+        info = [parse_paragraph(p) for p in paragraphs(s)]
+        print(info)
+
+
+if __name__ == "__main__":
+    assert len(sys.argv) > 1, "Missing file path"
+    main(sys.argv[1])
